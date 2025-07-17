64d3987c170605677d9b0a980b682ac486608baa
Simon Kovtyk <simon@kovtyk.com>
2025-07-17 09:30:13 +0200

    feat(number-input-directive): add key bypass

--- src/directives/input/number-input.directive.ts
+++ src/directives/input/number-input.directive.ts
@@ -1,16 +1,62 @@
 import { Directive, HostListener } from "@angular/core";
 import { KeyboardKeys } from "@ogs-gmbh/ngx-utils";
 
+type BypassKey = {
+  ctrl?: boolean;
+  name: string;
+};
+
 @Directive({
   selector: "input[numberInput]",
   standalone: true
 })
 export class NumberInputDirective {
-  private _bypassKeys: string[] = [ KeyboardKeys.BACKSPACE ];
+  private _bypassKeys: BypassKey[] = [
+    {
+      name: KeyboardKeys.BACKSPACE
+    },
+    {
+      ctrl: true, name: "v"
+    },
+    {
+      ctrl: true, name: "c"
+    },
+    {
+      ctrl: true, name: "x"
+    },
+    {
+      ctrl: true, name: "a"
+    },
+    {
+      ctrl: true, name: "z"
+    },
+    {
+      name: "ArrowRight"
+    },
+    {
+      name: "ArrowLeft"
+    },
+    {
+      name: "Tab"
+    }
+  ];
 
   @HostListener("keydown", [ "$event" ])
   public handleInput (keyboardEvent: KeyboardEvent): void {
-    if (this._bypassKeys.includes(keyboardEvent.key))
+    let bypass: boolean = false;
+
+    for (const bypassKey of this._bypassKeys) {
+      if (bypass)
+        break;
+
+      /* eslint-disable-next-line @unicorn/prefer-ternary */
+      if (bypassKey.ctrl)
+        bypass = bypassKey.ctrl === keyboardEvent.ctrlKey && bypassKey.name === keyboardEvent.key;
+      else
+        bypass = keyboardEvent.key === bypassKey.name;
+    }
+
+    if (bypass)
       return;
 
     const isDataNaN: boolean = Number.isNaN(Number(keyboardEvent.key));
